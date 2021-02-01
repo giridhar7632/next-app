b72d8f48e743eed4716ceaed29a57d2377f10141
giridhar7632 <giridhar.talla2002@gmail.com>
2021-02-01 09:42:39 +0530

    eslint ⚙

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,18 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello <span>World!</span> 👋
-        </h1>
-      </main>
-    </div>
-  )
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					Hello <span>World!</span> 👋
+				</h1>
+			</main>
+		</div>
+	)
 }
